c31ce628cd4a2fe0b5dc60389ac3c4330f5ca1e4
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2021-01-21 16:21:51 +0100

    Clase 4

--- platziHelloWorld/Program.cs
+++ platziHelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace platziHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola platzi");
         }
     }
 }
