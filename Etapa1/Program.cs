75cf7afa03c6d0273a0d711cdab4e7919224f420
Jose Ramon Vieites Romay <fabelaretorcida@gmail.com>
2021-01-22 09:16:32 +0100

    Clase 17

--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -36,11 +36,42 @@ namespace Etapa1
             Console.WriteLine($"{courses[1].Name} , {courses[1].Id}");
             Console.WriteLine(courses[2]);
             Console.WriteLine("==============");
-            ImprimirCursos(courses);
+            PrintCoursesWhile(courses);
+            Console.WriteLine("==============");
+            PrintCoursesDoWhile(courses);
+            Console.WriteLine("==============");
+            PrintCoursesFor(courses);
+            Console.WriteLine("==============");
+            PrintCoursesForEach(courses);
 
         }
 
-        private static void ImprimirCursos(Course[] courses)
+        private static void PrintCoursesForEach(Course[] courses)
+        {
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"Nombre: {course.Name}, Id {course.Id}");
+            }
+        }
+
+        private static void PrintCoursesFor(Course[] courses)
+        {
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine($"Nombre: {courses[i].Name}, Id {courses[i].Id}");
+            }
+        }
+
+        private static void PrintCoursesDoWhile(Course[] courses)
+        {
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"Nombre: {courses[counter].Name}, Id {courses[counter].Id}");
+            } while (++counter < courses.Length);
+        }
+
+        private static void PrintCoursesWhile(Course[] courses)
         {
             int counter = 0;
             while (counter < courses.Length)
